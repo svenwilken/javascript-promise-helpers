ff239bea7887993e442408cf18e39b7149912916
Sven Wilken <sven-lars.wilken@t-online.de>
2020-09-06 15:36:07 +0200

    add additional tests

--- test/array.spec.ts
+++ test/array.spec.ts
@@ -1,10 +1,13 @@
 import {
   mapP,
   forEachP,
-  someP,
-  everyP,
+  somePP,
+  somePS,
+  everyPP,
+  everyPS,
   filterP,
-  findP,
+  findPP,
+  findPS,
   reduceP,
   reduceRightP,
 } from "../lib/array";
@@ -35,27 +38,51 @@ describe("Promise Helper Array Functions", () => {
     });
   });
 
-  describe("someP", () => {
+  describe("somePP", () => {
     it("case 1", async () => {
       const array = [1, 3, 5, 7, 9, 11, 13, 15, 12];
-      expect(await someP(array, async (a) => a % 2 === 0)).toBe(true);
+      expect(await somePP(array, async (a) => a % 2 === 0)).toBe(true);
     });
 
     it("case 2", async () => {
       const array = [1, 3, 5, 7, 9, 11, 13, 15];
-      expect(await someP(array, async (a) => a % 2 === 0)).toBe(false);
+      expect(await somePP(array, async (a) => a % 2 === 0)).toBe(false);
     });
   });
 
-  describe("everyP", () => {
+  describe("somePS", () => {
     it("case 1", async () => {
       const array = [1, 3, 5, 7, 9, 11, 13, 15, 12];
-      expect(await everyP(array, async (a) => a % 2 === 1)).toBe(false);
+      expect(await somePS(array, async (a) => a % 2 === 0)).toBe(true);
     });
 
     it("case 2", async () => {
       const array = [1, 3, 5, 7, 9, 11, 13, 15];
-      expect(await everyP(array, async (a) => a % 2 === 1)).toBe(true);
+      expect(await somePS(array, async (a) => a % 2 === 0)).toBe(false);
+    });
+  });
+
+  describe("everyPP", () => {
+    it("case 1", async () => {
+      const array = [1, 3, 5, 7, 9, 11, 13, 15, 12];
+      expect(await everyPP(array, async (a) => a % 2 === 1)).toBe(false);
+    });
+
+    it("case 2", async () => {
+      const array = [1, 3, 5, 7, 9, 11, 13, 15];
+      expect(await everyPP(array, async (a) => a % 2 === 1)).toBe(true);
+    });
+  });
+
+  describe("everyPS", () => {
+    it("case 1", async () => {
+      const array = [1, 3, 5, 7, 9, 11, 13, 15, 12];
+      expect(await everyPS(array, async (a) => a % 2 === 1)).toBe(false);
+    });
+
+    it("case 2", async () => {
+      const array = [1, 3, 5, 7, 9, 11, 13, 15];
+      expect(await everyPS(array, async (a) => a % 2 === 1)).toBe(true);
     });
   });
 
@@ -74,15 +101,27 @@ describe("Promise Helper Array Functions", () => {
     });
   });
 
-  describe("findP", () => {
+  describe("findPP", () => {
     it("case 1", async () => {
       const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-      expect(await findP(array, async (a) => a % 13 === 0)).toBe(13);
+      expect(await findPP(array, async (a) => a % 13 === 0)).toBe(13);
     });
 
     it("case 2", async () => {
       const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-      expect(await findP(array, async (a) => a % 17 === 0)).toBeNull();
+      expect(await findPP(array, async (a) => a % 17 === 0)).toBeNull();
+    });
+  });
+
+  describe("findPS", () => {
+    it("case 1", async () => {
+      const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+      expect(await findPS(array, async (a) => a % 13 === 0)).toBe(13);
+    });
+
+    it("case 2", async () => {
+      const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+      expect(await findPS(array, async (a) => a % 17 === 0)).toBeNull();
     });
   });
 
